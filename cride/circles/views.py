376a3208ff762a7801f9095db6ea5430d10e74ac
mborjas <borjascastillo@hotmail.com>
2019-06-18 03:20:57 +0100

    implementar jronresponse rest api simple

--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,12 +1,28 @@
 """Circle views."""
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.http import JsonResponse
+
+from cride.circles.models import Circle
+
+# def list_circles(request):
+#     """LIst Circle """
+#     return HttpResponse('hola')
 
 def list_circles(request):
     """LIst Circle """
-    return HttpResponse('hola')
+    circles = Circle.objects.all()
+    public = circles.filter(is_public=True)
+    data = []
+    for circle in public: #aki se se hace el query (lazy)
+        # print(circle)
+        data.append({
+            'name':circle.name,
+            'slug_name':circle.slug_name,
+            'rides_taken':circle.rides_taken
+        })
 
-
+    return JsonResponse(data,safe=False)
 
 
 
