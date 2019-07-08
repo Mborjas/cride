1dad3582c6f301323920bfb623da7270dfb04474
mborjas <borjascastillo@hotmail.com>
2019-07-08 04:02:14 +0100

    agregar permisos y auth al api

--- config/settings/base.py
+++ config/settings/base.py
@@ -157,3 +157,18 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERYD_TASK_TIME_LIMIT = 5 * 60
 CELERYD_TASK_SOFT_TIME_LIMIT = 60
+
+
+
+# Django REST Framework
+REST_FRAMEWORK = {
+    # esto es para que no muestre la interfaz para hacer crud con drf
+    # 'DEFAULT_RENDERER_CLASSES': (
+    #     'rest_framework.renderers.JSONRenderer',
+    # ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3,
+}
