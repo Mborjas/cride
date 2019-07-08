f14778c040afda2493eb7f09c37197e225c1c2a5
mborjas <borjascastillo@hotmail.com>
2019-07-08 05:17:17 +0100

    agreagar permisos personalizados

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -2,7 +2,11 @@
 
 # Django REST Framework
 from rest_framework import viewsets
+from rest_framework import mixins, viewsets
+
+# Permissions
 from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.circles import IsCircleAdmin
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
@@ -11,13 +15,18 @@ from cride.circles.serializers import CircleModelSerializer
 from cride.circles.models import Circle, Membership
 
 
-class CircleViewSet(viewsets.ModelViewSet):
+# class CircleViewSet(viewsets.ModelViewSet):
+class CircleViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
     """Circle view set."""
 
     queryset = Circle.objects.all()
     serializer_class = CircleModelSerializer
 
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
 
     # muestra solo activos
     def get_queryset(self):
@@ -27,6 +36,13 @@ class CircleViewSet(viewsets.ModelViewSet):
             return queryset.filter(is_public=True)
         return queryset
     
+    def get_permissions(self):
+        """Assign permissions based on action."""
+        permissions = [IsAuthenticated]
+        # partial_update es PATCH
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsCircleAdmin)
+        return [permission() for permission in permissions]
 
     def perform_create(self, serializer):
         """Assign circle admin."""
